7655451edae05519f8dac90bc232b7441033a0d3
Hannah Shine <hshine@visitationacademy.org>
2018-06-21 14:24:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function discountTicket(){
 }
 
 function tweetReply (name){
-  console.log('Hey${name}, thanks so much for the support! I really appreciate it!`)
+  console.log `Hey${name}, thanks so much for the support! I really appreciate it!`
 }
